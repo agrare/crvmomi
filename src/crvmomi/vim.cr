9352ba75ad637fc5a81013be2149e4e61b2ec04d
Adam Grare <agrare@redhat.com>
2017-07-20 13:25:06 -0400

    Add VIM.connect class method

--- src/crvmomi/vim.cr
+++ src/crvmomi/vim.cr
@@ -3,7 +3,13 @@ require "./vmodl"
 module CrVmomi
   class VIM < Vmodl
     def self.connect(host, port = nil, ssl = true, insecure = false)
-      new(host, port, ssl, insecure)
+      new(host, port, ssl, insecure).tap do |vim|
+        vim.serviceInstance.retrieve_service_content
+      end
+    end
+
+    def serviceInstance
+      VIM::ServiceInstance.new(self, "ServiceInstance")
     end
   end
 end
